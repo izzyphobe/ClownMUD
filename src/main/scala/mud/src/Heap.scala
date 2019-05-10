970f2e4a9697ac59b8c0602f6f22a4dbd626e300
Izzy <izzy@alfheim.org>
2019-05-10 16:47:30 -0500

    heaping

--- src/main/scala/mud/src/Heap.scala
+++ src/main/scala/mud/src/Heap.scala
@@ -4,33 +4,44 @@ import scala.reflect.ClassTag
 
 class HeapQueue[A:ClassTag](priority:(A,A)=>Boolean){
   var arr:Array[A]=Array.fill(20)(null.asInstanceOf[A])
-  var bubble=1 //object moving up
-  var stone=2 //object moving down, ends at empty bottom node
+
   var length=0
   def enqueue(add:A)={
-    while(!(bubble==1||priority(arr(bubble-1),add))){
-      arr(bubble)=arr(bubble/2)
-      bubble-=1
-      
+    if(length>arr.length){
+      var newArr=Array.fill(arr.length*2)(null.asInstanceOf[A])
+      for(v<-0 to arr.length){
+        newArr(v)=arr(v)
+      }
     }
-    
-//    if(priority(arr(stone-1),add)){
-//      arr(stone)=add
-//      stone+=1
-//    } else{
-//      if(priority(arr((stone-1)/2),add)){
-//        
-//      }
-//    }
+    length+=1
+    var idx=length
+    arr(idx)=add
+
+    while(idx>1&&priority(arr(idx),arr(idx/2))){
+      var tmp=arr(idx)
+      arr(idx)=arr(idx/2)
+      arr(idx/2)=tmp
+    }
   }
-  def dequeue={
-    
+  def dequeue:A={
+    var i=1
+    val toret=peek
+    while(arr.length>i*2){ //while the length is greater than the index of the left child. so, while there is a child at i
+      if(priority(arr(i*2),arr(i*2+1))){ //if left child at index > right child of index
+        arr(i)=arr(i*2)  //current index data = left child data
+        i*=2  //bubble the left child
+      }
+      else i=i*2+1 //bubble the right child
+    }
+
+    length=length-1
+    toret
   }
   def peek=arr(1)
-  
+
   def isEmpty= {
-    stone-1==bubble
+    length==0
   }
 
-    
+
 }
